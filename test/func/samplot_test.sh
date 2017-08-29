2bd39c433a88f4fc1b6772638fbcb4e1e9d49710
jrbelyeu <jrbelyeu@gmail.com>
2017-08-29 13:40:16 -0600

    added a simple functional test

--- test/func/samplot_test.sh
+++ test/func/samplot_test.sh
@@ -5,3 +5,11 @@ test -e ssshtest || wget -q https://raw.githubusercontent.com/ryanlayer/ssshtest
 . ssshtest
 
 STOP_ON_FAIL=0
+
+run basic_operation python ../../src/samplot.py -c 2 -s 89161083 -e 89185670 \
+    -b "../data/low_coverage/NA12878.mapped.ILLUMINA.bwa.CEU.low_coverage.restricted_sv_regions.20121211.bam" \
+"../data/low_coverage/NA12889.mapped.ILLUMINA.bwa.CEU.low_coverage.restricted_sv_regions.20130415.bam" \
+"../data/low_coverage/NA12890.mapped.ILLUMINA.bwa.CEU.low_coverage.restricted_sv_regions.20130415.bam" -o "test.jpg"
+assert_exit_code 0
+rm "test.jpg"
+rm "ssshtest"
