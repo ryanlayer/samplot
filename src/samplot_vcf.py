b5e9356ba40ae981c154453cdf13c0624442c8c0
Brent Pedersen <bpederse@gmail.com>
2018-12-03 15:49:36 -0700

    add ped file stuffs

--- src/samplot_vcf.py
+++ src/samplot_vcf.py
@@ -6,6 +6,44 @@ import os
 
 HERE = os.path.dirname(__file__)
 
+class Sample(object):
+    __slots__ = 'family_id id paternal_id maternal_id mom dad kids i'.split()
+    def __init__(self, line):
+        toks = line.rstrip().split("\t")
+        self.family_id = toks[0]
+        self.id = toks[1]
+        self.paternal_id = toks[2]
+        self.maternal_id = toks[3]
+        self.kids = []
+        self.i = -1 # index in the vcf.
+
+def parse_ped(path, vcf_samples=None):
+    samples = []
+    look = {}
+    for line in open(path):
+        samples.append(Sample(line))
+        look[samples[-1].id] = samples[-1]
+
+    for s in samples:
+        s.dad = look.get(s.paternal_id)
+        if s.dad is not None:
+            s.dad.kids.append(s)
+        s.mom = look.get(s.maternal_id)
+        if s.mom is not None:
+            s.mom.kids.append(s)
+    # match these samples to the ones in the VCF.
+
+    if vcf_samples is not None:
+        result = []
+        for i, vs in enumerate(vcf_samples):
+            if not vs in look: continue
+            result.append(next(s for s in samples if s.id == vs))
+            result[-1].i = i
+        samples = result
+
+    return {s.id: s for s in samples}
+
+
 def xopen(path): return gzip.open(path) if path.endswith(".gz") else open(path)
 
 def get_names_to_bams(bams):
@@ -24,6 +62,11 @@ def main(args, pass_through_args):
     vcf = pysam.VariantFile(args.vcf)
     vcf_samples = vcf.header.samples
 
+    if args.ped:
+        ped_samples = parse_ped(args.ped, vcf_samples)
+    else:
+        ped_samples = {}
+
     if not os.path.exists(args.out_dir):
       os.makedirs(args.out_dir)
 
@@ -41,8 +84,25 @@ def main(args, pass_through_args):
 
         vsamples = [vcf_samples[i] for i in idxs]
         bams = [names_to_bams[s] for s in vsamples]
+        # try to get family members
+        if args.ped is not None:
+            for vs in vsamples:
+                s = ped_samples.get(vs)
+                if s is None: continue
+                if s.mom is not None and not s.mom.id in vsamples and s.mom.id in vcf_samples:
+                    vsamples.append(s.mom.id)
+                    bams.append(names_to_bams[s.mom.id])
+                if s.dad is not None and not s.dad.id in vsamples and s.dad.id in vcf_samples:
+                    vsamples.append(s.dad.id)
+                    bams.append(names_to_bams[s.dad.id])
+                for kid in s.kids:
+                    if not kid.id in vsamples and kid.id in vcf_samples:
+                        vsamples.append(kid.id)
+                        bams.append(names_to_bams[kid.id])
 
-        if len(bams) < 6:
+            if len(bams) > 1.5 * args.max_hets: break
+
+        elif len(bams) < 6:
             # extend with some controls:
             hom_ref_idxs = [i for i, gt in enumerate(gts) if len(gt) == 2 and gt[0] == 0 and gt[1] == 0]
             if len(hom_ref_idxs) > 3:
@@ -73,6 +133,7 @@ if __name__ == "__main__":
     p = argparse.ArgumentParser("note that additional arguments are passed through to samplot.py")
     p.add_argument("--vcf", "-v", help="VCF file containing structural variants")
     p.add_argument("-d", "--out-dir", help="path to write output PNGs", default="samplot-out")
+    p.add_argument("--ped", help="path ped (or .fam) file")
     p.add_argument("--min-call-rate", type=float, help="only plot variants with at least this call-rate", default=0.9)
     p.add_argument("-O", "--output-type", choices=("png", "pdf", "eps", "jpg"), help="type of output figure", default="png")
     p.add_argument("--max-hets", type=int, help="only plot variants with at most this many heterozygotes", default=10)
