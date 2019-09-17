827ae1701c9e2368e8bbe64026cab5cc0b2edeae
Jonathan Belyeu <jrbelyeu@gmail.com>
2019-09-17 10:39:19 -0600

    another coverage bug dies

--- src/samplot.py
+++ src/samplot.py
@@ -174,7 +174,7 @@ def add_coverage(bam_file, read, coverage, minq):
     Quality is determined by minq, which is min quality
     """
 
-    chrm = bam_file.get_reference_name(read.reference_id)
+    chrm = bam_file.get_reference_name(read.reference_id).strip("chr")
 
     hp = 0
 
@@ -2431,7 +2431,6 @@ def get_read_data(ranges,
                 curr_max = max(max(coverage[chrm][pos].values()))
                 if curr_max > max_coverage:
                     max_coverage = curr_max
-
         all_coverages.append(coverage)
         all_pairs.append(pairs)
         all_splits.append(splits)
