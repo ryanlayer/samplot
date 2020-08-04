fa11fd52065cc21ff4aec45407ebb56a611d08fa
jbelyeu <jrbelyeu@gmail.com>
2020-08-04 10:04:00 -0600

    added -y option

--- .circleci/setup.sh
+++ .circleci/setup.sh
@@ -38,6 +38,7 @@ if [[ ! -d $WORKSPACE/anaconda ]]; then
 
     mkdir -p $WORKSPACE/anaconda/conda-bld/$tag-64
     conda create -n test python="$pythonversion.7" -y
+    conda activate test
 
     # step 2: setup channels
     conda config --system --add channels defaults
